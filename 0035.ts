5ad71797b0384b8d680d9045c064df163b3aad87
pfgallagher <pfgallagher@gmail.com>
2020-09-23 07:43:02 -0500

    Complete 0035

---
+++ 0035.ts
@@ -0,0 +1,8 @@
+const searchInsert = (nums: number[], target: number): number => {
+	for (let i = 0; i < nums.length; i++) {
+		if (target <= nums[i]) {
+			return i;
+		}
+	}
+	return nums.length;
+};

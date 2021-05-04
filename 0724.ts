94d55412aace0a25d51885cba135a0f2b91892f5
pfgallagher <pfgallagher@gmail.com>
2021-05-04 17:38:53 -0500

    Complete 0724

---
+++ 0724.ts
@@ -0,0 +1,12 @@
+export {};
+
+const sum = (nums: number[]): number => nums.reduce((a, c) => a + c, 0);
+
+const pivotIndex = (nums: number[]): number => {
+	for (const i of nums.keys()) {
+		if (sum(nums.slice(0, i)) === sum(nums.slice(i + 1))) {
+			return i;
+		}
+	}
+	return -1;
+};

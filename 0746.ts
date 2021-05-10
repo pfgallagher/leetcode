f2af34fff30d281bdbe4d0ecc842fc90e4012101
pfgallagher <pfgallagher@gmail.com>
2021-05-10 18:42:02 -0500

    Complete 0746

---
+++ 0746.ts
@@ -0,0 +1,10 @@
+const minCostClimbingStairs = (cost: number[]): number => {
+	const minCost: number[] = Array(cost.length + 1).fill(0);
+	for (let i = 2; i < minCost.length; i++) {
+		minCost[i] = Math.min(
+			minCost[i - 1] + cost[i - 1],
+			minCost[i - 2] + cost[i - 2],
+		);
+	}
+	return minCost[minCost.length - 1];
+};
